26720728f0039fc81498228ec155b5fc7425f851
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2023-08-02 14:56:26 +0200

    Overload constructor

--- dualdiff/dual.py
+++ dualdiff/dual.py
@@ -1,6 +1,19 @@
+from plum import dispatch
+from numbers import Number
+
 class Dual:
 
-    def __init__(self, x, dx):
-        """ Object constructor """
+    @dispatch
+    def __init__(self, x: Number, dx: Number = 0):
+        """ Basic constructor """
         self.x = x
         self.dx = dx
+
+    @dispatch
+    def __init__(self, z: "Dual"):
+        """ Additional constructor
+        If the input is already a dual number, just return it.
+        This will be handy later. """
+        self = z
+
+    
--- tests/test_dual.py
+++ tests/test_dual.py
@@ -2,6 +2,13 @@ from dualdiff.dual import Dual
 
 def test_init():
     z = Dual(2, 1)
-
     assert(z.x == 2)
     assert(z.dx == 1)
+
+    z = Dual(1)
+    assert(z.x == 1)
+    assert(z.dx == 0)
+
+    u = Dual(z)
+    assert (z.x == 1)
+    assert (z.dx == 0)
